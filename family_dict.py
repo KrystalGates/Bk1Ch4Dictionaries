f008f86d43d86d934ca07a4339ba06e17fcf5374
krystal gates <krystal_sabrina@yahoo.com>
2019-08-26 15:17:38 -0500

    finish practice

---
+++ family_dict.py
@@ -0,0 +1,38 @@
+# Instructions
+# 1. Define a dictionary that contains information about several members of your family. Use the following example as a template.
+
+# my_family = {
+#     "sister": {
+#         "name": "Krista",
+#         "age": 42
+#     },
+#     "mother": {
+#         "name": "Cathie",
+#         "age": 70
+#     }
+# }
+
+my_family = {
+    "sister": {
+        "name": "Sheri",
+        "age": 25
+    },
+    "mother": {
+        "name": "Juliann",
+        "age": 55
+    },
+    "father": {
+        "name": "Ronald",
+        "age": 73
+    }
+}
+# 2. Using a dictionary comprehension, produce output that looks like the following example.
+
+for key, value in my_family.items():
+    age_string = str(value["age"])
+    print(f'{value["name"]} is my {key} and is {age_string} years old')
+
+
+
+# Krista is my sister and is 42 years old
+# Helpful hint: To convert an integer into a string in Python, it's str(integer_value)
